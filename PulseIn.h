474993e4969277084aac3e67a68d5d6d817aed36
Dan <doates625@gmail.com>
2019-10-18 12:49:26 -0700

    Moved description to README

--- PulseIn.h
+++ PulseIn.h
@@ -2,17 +2,6 @@
  * @file PulseIn.h
  * @brief Class for Arduino and Mbed pulse-length reading inputs
  * @author Dan Oates (WPI Class of 2020)
- * 
- * This class adds pulse-length reading functionality to the DigitalIn class.
- * It is capable of reading the lengths of both low and high pulses with a 
- * configurable timeout, after which the method will return 0.
- * 
- * Dependencies:
- * - DigitalIn: https://github.com/doates625/DigitalIn.git
- * - Platform: https://github.com/doates625/Platform.git
- * 
- * References:
- * - Arduino pulseIn: https://www.arduino.cc/reference/en/language/functions/advanced-io/pulsein/
  */
 #pragma once
 #include <DigitalIn.h>
---
+++ README.md
@@ -0,0 +1,10 @@
+# PulseIn
+Class for Arduino and Mbed pulse-length reading inputs  
+Written by Dan Oates (WPI Class of 2020)
+
+### Description
+This class adds pulse-length reading functionality to the DigitalIn class. It is capable of reading the lengths of both low and high pulses with a configurable timeout, after which the method will return 0.
+
+### Dependencies
+- [Platform](https://github.com/doates625/Platform.git)
+- [DigitalIn](https://github.com/doates625/DigitalIn.git)
